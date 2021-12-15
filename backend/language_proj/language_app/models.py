221a67c542d1094ec0dd79f107cc36d2bc42a58e
Brandon <williambwatson42@gmail.com>
2021-12-15 13:06:28 -0600

    added creation date field to profile model

--- backend/language_proj/language_app/models.py
+++ backend/language_proj/language_app/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 
 class Language(models.Model):
     name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
 
 class Profile(models.Model):
     '''
@@ -12,9 +14,12 @@ class Profile(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     friends = models.ManyToManyField(User, related_name="friends", blank=True)
-
+    creation_date = models.DateField(auto_now=True)
     native_language = models.ForeignKey(Language, related_name="natives", on_delete=models.DO_NOTHING)
     language_learning = models.ForeignKey(Language, related_name="learners", on_delete=models.DO_NOTHING)
 
     discord_name = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.user
 
