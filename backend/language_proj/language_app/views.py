cbfcb2de66da23c54e72bcbb7f4922f997e055d5
Brandon <williambwatson42@gmail.com>
2021-12-15 14:21:50 -0600

    added modelviewsets of profile language and user

--- backend/language_proj/language_app/views.py
+++ backend/language_proj/language_app/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from .serializers import UserSerializer, ProfileSerializer, LanguageSerializer
+from .models import Profile, Language
+from django.contrib.auth.models import User
+from rest_framework.viewsets import ModelViewSet
 
 
+class ProfileViewSet(ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class LanguageViewSet(ModelViewSet):
+    queryset = Language.objects.all()
+    serializer_class = LanguageSerializer
 
