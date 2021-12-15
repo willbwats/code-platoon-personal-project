f6be493574a715e2f71388ed5e315a0b83c68402
Brandon <williambwatson42@gmail.com>
2021-12-15 12:02:55 -0600

    added profile and language models

--- backend/language_proj/language_app/models.py
+++ backend/language_proj/language_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    '''
+    Make a profile when you make a user
+    '''
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    friends = models.ManyToManyField(Profile, related_name="friends", blank=True)
+
+    native_language = models.ForeignKey(Language, related_name="natives", blank=True)
+    second_native_language = models.ForeignKey(Language, related_name="second_language_natives", blank=True)
+
+    language_learning = models.ForeignKey(Language, related_name="learners")
+
+    discord_name = models.CharField(max_length=255, blank=True)
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=255)
