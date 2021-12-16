5f5251fd8d0729690dfa4d08fb33a2dd5d23f0f1
Brandon <williambwatson42@gmail.com>
2021-12-16 09:20:37 -0600

    removed some boiler plate react files

--- frontend/language_exchange_site/src/App.js
+++ frontend/language_exchange_site/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+//pages
+import home from './pages/home';
+
 
 function App() {
   return (
     <div className="App">
-      Hi
+      <BrowserRouter>
+        <Routes>
+            <Route exact path='/' component={home} />
+            <Route exact path='/user/:profileID' component={profile} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
